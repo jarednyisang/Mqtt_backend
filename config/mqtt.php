0fded20a387e9b7fbec1a714a90c2b3a9402da69
Jared Nyisang <jarednyisang@gmail.com>
2025-12-16 11:58:02 +0300

    good progress

--- config/mqtt.php
+++ config/mqtt.php
@@ -5,6 +5,5 @@ return [
     'port' => env('MQTT_PORT', 1883),
     'username' => env('MQTT_USERNAME', null),
     'password' => env('MQTT_PASSWORD', null),
-    'client_id' => env('MQTT_CLIENT_ID', 'laravel_' . uniqid()),
-    'clean_session' => env('MQTT_CLEAN_SESSION', true),
+    'client_id' => env('MQTT_CLIENT_ID', 'chloride_laravel'),
 ];
