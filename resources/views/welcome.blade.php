56771f29b0f35eeafeff5b8ac9c72d1e4b189fe2
Jared Nyisang <jarednyisang@gmail.com>
2025-12-16 08:47:48 +0300

    good progress

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,9 +10,9 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <style>
         :root {
-            --primary: #e54646ff;
-            --secondary: #12076eff;
-            --dark: #1e293b;
+            --primary: #e55146ff;
+            --secondary: #210da8ff;
+            --dark: #210da8ff;
         }
 
         body {
